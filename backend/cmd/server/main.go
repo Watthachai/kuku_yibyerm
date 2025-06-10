a5c73dd7cea851bdefab5f3ab959a3418cd5b40d
Watthachai <wattchaichai@gmail.com>
2025-06-10 17:32:59 +0700

    < INFO > : Add Dockerfile for automatic deploy for backend in railway

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from the Go backend!")
+}
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // Default port for local development
+	}
+
+	http.HandleFunc("/api", helloHandler)
+
+	log.Println("Starting server on port:" + port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
